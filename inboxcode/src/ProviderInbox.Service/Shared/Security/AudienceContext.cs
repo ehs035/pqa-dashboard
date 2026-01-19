4ea8dc33ebf7be3bb8d8a53bc1cf4279ab6dc238
DESKTOP-TU5KJ07\ehs03 <ehs035@msn.com>
2026-01-18 17:26:05 -0800

    try again

--- inboxcode/src/ProviderInbox.Service/Shared/Security/AudienceContext.cs
+++
@@ -1,3 +0,0 @@
-namespace ProviderInbox.Service.Shared.Security;
-public sealed record AudienceKey(string Npi, string OfficeNumber);
-public interface IAudienceContext { AudienceKey GetAudience(); }
